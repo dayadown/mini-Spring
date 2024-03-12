f10d7a26ada877954cc2f7361cd9f59be65837d5
dayadown <1593441796@qq.com>
2024-03-12 13:03:50 +0800

    使用实例化策略创建bean

--- src/main/java/com/my/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/com/my/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -8,6 +8,8 @@ import com.my.beans.factory.config.BeanDefinition;
  * 有自动装配能力的抽象bean容器，实现了抽象bean容器的创建bean的方法，没有实获取bean信息的方法，所以还是抽象类
  */
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
+	//使用构造哈函数策略创建实例
+	private InstantiationStrategy instantiationStrategy = new SimpleInstantiationStrategy();
 
 	@Override
 	protected Object createBean(String beanName, BeanDefinition beanDefinition) throws BeansException {
@@ -24,13 +26,35 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
 		Class beanClass = beanDefinition.getBeanClass();
 		Object bean = null;
 		try {
-			//调用beanClass的无参构造
-			bean=beanClass.newInstance();
+			//调用createBeanInstance创建实例
+			//bean=createBeanInstance(beanDefinition);
+			bean=instantiationStrategy.instantiate(beanDefinition);
 		} catch (Exception e) {
 			throw new BeansException("Instantiation of bean failed", e);
 		}
 
 		addSingleton(beanName, bean);
 		return bean;
+	}
+
+	/**
+	 * 根据bean信息创建实例
+	 * @param beanDefinition
+	 * @return
+	 */
+	protected Object createBeanInstance(BeanDefinition beanDefinition) {
+		return getInstantiationStrategy().instantiate(beanDefinition);
+	}
+
+	/**
+	 * instantiationStrategy的get和set方法
+	 * @return
+	 */
+	public InstantiationStrategy getInstantiationStrategy() {
+		return instantiationStrategy;
+	}
+
+	public void setInstantiationStrategy(InstantiationStrategy instantiationStrategy) {
+		this.instantiationStrategy = instantiationStrategy;
 	}
 }
